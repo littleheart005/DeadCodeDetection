23e397339fa64a785ba8cb5450fe5203155a005b
Peeradon <pee-radon@hotmail.com>
2020-08-24 17:31:16 +0700

    Modify Deadclass_detector -> getting class/interface name successful and mapping class name into LinkedHashMap.

--- src/main/java/DeadClass/DeadClass_Detector.java
+++ src/main/java/DeadClass/DeadClass_Detector.java
@@ -1,16 +1,15 @@
 package DeadClass;
 
 import Files_Reader.Dead_Class_Reader;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class DeadClass_Detector {
 
     /* Steps for detecting dead class/interface
-     - read file
-     - get class/interface name ( Except "main" )
+     - read file --> Done !!
+     - get class/interface name ( Except "main" ) --> Done !!
+     - create map for storing result <(String)Class_name, (Boolean)Deadcode?> --> Done !!
+
      - check through every file except itself :
         case I : if count = 0 --> Dead!
         case II : if count >= 1 --> Alive.
@@ -19,46 +18,48 @@ public class DeadClass_Detector {
     */
 
     private List<String> paths;
-    private List<String> name;
+    private List<String> names;
+    private Map<String, Boolean> maps;
 
     public DeadClass_Detector(String source){
 
+        // Create reader
         Dead_Class_Reader reader = new Dead_Class_Reader();
 
-        System.out.println("============== Path from Reader ===============");
-
+        // Store path
         paths = reader.readPath(source);
 
-        //name = getClassName();
+        // Store name
+        names = reader.readFile_Name(paths);
 
-        System.out.println("************** Path in detector ******************");
-        for(String p : paths){
+        System.out.println("************** Classes name ******************");
+        for(String p : names){
             System.out.println(p);
         }
 
+        // Map name from String to map
+        maps = toMap(names);
+
     }
 
-    public List<String> getClassName(){
-        List<String> tmpName = new ArrayList<>();
-        for(String p : paths) {
-            File file = new File(p);
-            String tmp;
-            tmp = getFileName(file);
-            tmpName.add(tmp);
+    public Map<String, Boolean> toMap(List<String> strings){
+
+        Map<String, Boolean> maps = new LinkedHashMap<>();
+        for(int i=0;i<strings.size();i++){
+            maps.put(strings.get(i),false);
         }
-        return tmpName;
+
+        System.out.println("========== Linked Hash Maps ========");
+        for(Map.Entry<String ,Boolean> m : maps.entrySet()) {
+            System.out.print(m.getKey() + " : ");
+            System.out.println(m.getValue());
+        }
+
+        return maps;
     }
 
-    public String getFileName(File file){
-        return removeExtension(file.getName());
-    }
+    public void detect(){
 
-    public static String removeExtension(String file){
-        return file.replaceFirst("[.][^.]+$", "");
-    }
-
-    public List<String> getName() {
-        return name;
     }
 
 }
--- src/main/java/Files_Reader/Dead_Class_Reader.java
+++ src/main/java/Files_Reader/Dead_Class_Reader.java
@@ -13,28 +13,52 @@ public class Dead_Class_Reader extends File_Reader{
         return super.readPath(source);
     }
 
-    @Override
-    public void readFile(List<String> paths) {
+    public List<String> readFile_Name(List<String> paths) {
         /* Next step, this method should return a list 'word' to caller
          * for using in various method */
+        List<String> names = new ArrayList<>();
+        for(String p : paths){
+
+            File file = new File(p);
+            String tmp;
+            tmp = getFileName(file);
+            names.add(tmp);
+        }
+        return names;
+    }
+
+    public String getFileName(File file){
+        return removeExtension(file.getName());
+    }
+
+    public static String removeExtension(String file){
+        return file.replaceFirst("[.][^.]+$", "");
+    }
+
+    @Override
+    public void readFile(List<String> paths) {
         for(String p : paths){
             File file = new File(p);
-            List<String> words = new ArrayList<>(); // create array to store all words in file
-
             try {
                 Scanner sc = new Scanner(file);
                 while (sc.hasNext()){
+                    /*Scanner sc = new Scanner(file);
+                    while (sc.hasNext()){
                     String word = sc.next();
-                    words.add(word); // add words to array
+                    words.add(word); // add words to array */
+                    //searchClassName(words);
+                    sc.close();
                 }
-                searchClassName(words);
             }catch (FileNotFoundException e){
                 e.printStackTrace();
             }
-
         }
     }
 
+
+
+    // Search the names by convert the file to txt and then search for name after the word
+    // "Class" or "Interface" in the file.
     public void searchClassName(List<String> words){
         String name = search(words);
         System.out.println("Class/Interface name is : " + name);
@@ -49,5 +73,4 @@ public class Dead_Class_Reader extends File_Reader{
         }
         return Name;
     }
-
 }
